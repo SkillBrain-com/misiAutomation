6893e2070b69f591414ea49c28bdc978e702fd78
LegionsX-Marius <mariusbun@gmail.com>
2024-11-05 20:59:04 +0200

    UpdateProject1

--- src/main/java/org/misi/ro/Login.java
+++ src/main/java/org/misi/ro/Login.java
@@ -6,6 +6,7 @@ public class Login {
 
     String name;
     String password;
+    String varsta;
 
     public void userName(){
         System.out.println("Introduceti Usename");
@@ -13,5 +14,8 @@ public class Login {
 
     public void setPassword(){
         System.out.println("Introduceti parola");
+    }
+    public void setVarsta(){
+        System.out.println("Varsta este: ");
     }
 }
