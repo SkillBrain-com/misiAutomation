8aba99710f2cab58ae5cb3184c186e9cbe8bfe58
gdunareanu <gdunareanu@yahoo.com>
2024-11-07 09:34:38 -0800

    primul meu proiect

---
+++ src/main/java/org/misi/ro/Georgiana.java
@@ -0,0 +1,22 @@
+package org.misi.ro;
+
+public class Georgiana{
+    public static void main(String[] args) {
+    }
+    String name;
+    String prenume;
+    String varsta;
+
+    public void setName(){
+        System.out.println("Maria");
+    }
+
+    public void setPrenume() {
+        System.out.println("Maria");
+    }
+
+    public void setVarsta() {
+        System.out.println("30");
+    }
+}
+
